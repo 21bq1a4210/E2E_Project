7bdb9643816a0b51ea19ba200405e075e2b7e6c2
BhargavBunty <bhargavbunty@059gmail.com>
2024-10-03 14:02:59 +0530

    minor refactor

--- ai_administration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ai_administration'
-
-urlpatterns = [
-    path('administration/', views.complaint_administration, name='ai_administration')
-]
-
